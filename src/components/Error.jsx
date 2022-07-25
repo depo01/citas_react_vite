230c5019b8dc0f22c50d2a90c926fcacd1d20f97
depo01 <josuebatista01p@gmail.com>
2022-07-25 08:41:57 +0200

    Primer commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+
+const Error = ({children}) => {
+    return (
+    <div className='bg-red-800 text-center text-white p-3 uppercase font-bold rounded-md mb-3 shadow-sm'>
+        {children}
+    </div>
+    )
+}
+
+export default Error
